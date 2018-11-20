ca7db6a6bc9abd2abc00bab476258b8960acd93b
7seven7lst <shengtaoli0110@gmail.com>
2018-11-19 20:07:42 -0800

    [update] client should not see the infra and data manager

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, IndexLink } from 'react-router';
 import { Menu, Button } from 'semantic-ui-react';
 
@@ -12,12 +12,17 @@ export default ({ children }) => {
           <Menu.Item position="left">
             <IndexLink to="/">Home</IndexLink>
           </Menu.Item>
-          <Menu.Item position="left">
-            <Link to="/infra-manager">Infra Manager</Link>
-          </Menu.Item>
-          <Menu.Item position="left">
-            <Link to="/data-manager">Data Manager</Link>
-          </Menu.Item>
+          { Auth.getUser()!=='client' &&
+            <Fragment>
+              <Menu.Item position="left">
+                <Link to="/infra-manager">Infra Manager</Link>
+              </Menu.Item>
+              <Menu.Item position="left">
+                <Link to="/data-manager">Data Manager</Link>
+              </Menu.Item>
+            </Fragment>
+          }
+
           <Menu.Item position="right">
             <Link to="/logout"><Button primary>Logout</Button></Link>
           </Menu.Item>

4b63704c1293f58a3d001dbe90311b80139d3ed0
7seven7lst <shengtaoli0110@gmail.com>
2018-12-02 23:29:30 -0800

    [update] use auth

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ passport.use('local-login', localLoginStrategy);
 
 // pass the authorization checker middleware
 const authCheckMiddleware = require('./middleware/auth-check');
-//app.use('/api', authCheckMiddleware);
+app.use('/api', authCheckMiddleware);
 
 // routes
 const authRoutes = require('./routes/auth-routes');

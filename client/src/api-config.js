f26269e224bcc5222d4e42f857488c519686765d
7seven7lst <shengtaoli0110@gmail.com>
2018-12-08 18:03:07 -0800

    [update] update

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -1,7 +1,7 @@
-export const AUTH_HOST = 'http://ec2-54-91-30-44.compute-1.amazonaws.com:4001';
-export const SIMULATION_HOST = 'http://ec2-54-91-30-44.compute-1.amazonaws.com:4001/api/v1';
-export const INFRA_MANAGER_HOST = 'http://ec2-54-91-30-44.compute-1.amazonaws.com:4001/api/v1';
-export const DATA_MANAGER_HOST = 'http://ec2-54-91-30-44.compute-1.amazonaws.com:4001/api/v1';
+export const AUTH_HOST = 'http://my-load-balancer-1898971536.us-east-1.elb.amazonaws.com';
+export const SIMULATION_HOST = 'http://my-load-balancer-1898971536.us-east-1.elb.amazonaws.com/api/v1';
+export const INFRA_MANAGER_HOST = 'http://my-load-balancer-1898971536.us-east-1.elb.amazonaws.com/api/v1';
+export const DATA_MANAGER_HOST = 'http://my-load-balancer-1898971536.us-east-1.elb.amazonaws.com/api/v1';
 
 //export const SEARCH_BY_GEOCODE = '/buildings/search/geocode';
 // used in src/component/Map.js
--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 sudo apt-get update -y
-sudo apt-get install -y docker
-sudo service docker start
-
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 sudo apt-get install -y git
 
 git clone https://github.com/Green-Building/dashboard.git
-
-sudo docker build -t dash/dashboard dashboard/
-
-sudo docker run -d -p 4001:4001 dash/dashboard
+cd dashboard
+npm install
+nohup node index &
